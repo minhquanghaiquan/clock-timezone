b786258b98647108571d78d3cc5849f0332ea852
minhquanghaiquan <62640640+minhquanghaiquan@users.noreply.github.com>
2020-06-23 09:10:19 +0200

    changeReadme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # clock-timezone1
-Created with CodeSandbox
+Hello!
+
+This is clock-timezone app. 
+
+You can select add and delete another timezone, default this this timezone CMT++00:00
+
 https://codesandbox.io/embed/github/minhquanghaiquan/clock-timezone1/tree/master/?fontsize=14&hidenavigation=1&theme=dark
--- src/App.js
+++ src/App.js
@@ -1,69 +1,56 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-import AnalogClock, { Themes } from 'react-analog-clock';
-import TimeZone from './TimeZone.js'
-import moment from 'moment'
+import AnalogClock, { Themes } from "react-analog-clock";
+import TimeZone from "./components/TimeZone.js";
+import moment from "moment";
 
-require('moment-timezone');
-
-
-
-
-
+require("moment-timezone");
 
 export default function App() {
-
-
   const [timezone, setTimezone] = useState([
-    { value: '0.0', label: '(GMT+00:00) London": "Europe/London"', location:'Europe/London' }
+    {
+      value: "0.0",
+      label: '(GMT+00:00) London": "Europe/London"',
+      location: "Europe/London"
+    }
   ]);
-  
 
+  const handleTimeZone = e => {
+    setTimezone([...timezone, e]);
+  };
 
-  const handleTimeZone = (e) => {
-    setTimezone([...timezone, e])
-  }
-  
-  const handleClick = (zone) => {
-    const index = timezone.findIndex(x => x.label===zone.label)
-    const copyArray = [...timezone]
+  const handleClick = zone => {
+    const index = timezone.findIndex(x => x.label === zone.label);
+    const copyArray = [...timezone];
     copyArray.splice(index, 1);
     setTimezone(copyArray);
-  }
-
+  };
 
   let m = moment();
-  
+
   return (
     <div className="App">
-        
-        <TimeZone handleTimeZone={handleTimeZone}/>
-       <div className="container">
-       {
-       
-          timezone.map((zone)=> (
-            <>
-              <div className="item">
-                <div className="clock">
-                  <AnalogClock 
-                  theme={Themes.dark} 
-                  gmtOffset={zone.value}
-                    /> 
-                 </div>
-                  <label className="label-location">{m.tz(zone.location).format('YYYY-MM-DD')}</label>
-                  <br/>
-                  <label className="label-zone">{zone.label}</label>
-                  <br/>
-                  <button className="button"
-                    onClick= {()=> handleClick(zone)}>Delete
-                  </button> 
+      <TimeZone handleTimeZone={handleTimeZone} />
+      <div className="container">
+        {timezone.map(zone => (
+          <>
+            <div className="item">
+              <div className="clock">
+                <AnalogClock theme={Themes.dark} gmtOffset={zone.value} />
               </div>
-            </> 
-            ) 
-          )
-       }
-       </div>
+              <label className="label-location">
+                {m.tz(zone.location).format("YYYY-MM-DD")}
+              </label>
+              <br />
+              <label className="label-zone">{zone.label}</label>
+              <br />
+              <button className="button" onClick={() => handleClick(zone)}>
+                Delete
+              </button>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
-
